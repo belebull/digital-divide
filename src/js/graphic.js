ef9ca8038bbbfdbf5d5dc2ff1c031f8009dd5e16
Beleicia Bullock <beleiciabullock@DNa1c05db.SUNet>
2021-11-25 14:14:40 -0800

    mapped usage data to visualization

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -39,25 +39,26 @@ function fixDiscrepancies(data) {
     counties.features[topoIdx] = topoCounty;
   });
 
-  return counties;
+  return { counties, broadband };
 }
 
 function generateCountyData(data) {
-  const { counties, states, broadband } = data;
+  // clean county data
+  const { counties, broadband } = fixDiscrepancies(data);
 
   // update county information
   const updatedCounties = counties.features.map((county) => {
-    const countyState = states.find(
-      (state) => state.STATE === county.properties.STATEFP
+    const { abbr, id, name, availability, usage } = broadband.find(
+      (broadbandCounty) => +broadbandCounty.id === +county.id
     );
-
-    const name = `${county.properties.NAME} County`;
 
     return {
       ...county,
       properties: {
         name,
-        state: countyState.STUSAB,
+        state: abbr,
+        availability: +availability,
+        usage: +usage,
       },
     };
   });
@@ -71,7 +72,8 @@ USAGE: creates a cartogram of the U.S. with oulinted counties
 SOURCES:
   - County Boundaries by Ian Johnson on Observable (https://observablehq.com/@enjalot/county-boundaries)
 */
-function createCartogram(counties) {
+function createCartogram(data) {
+  const { us, counties } = data;
   // set dimensions of map container for projection
   const width =
     document.getElementsByClassName('chart-cartogram')[0].offsetWidth;
@@ -86,31 +88,47 @@ function createCartogram(counties) {
     .attr('height', height);
 
   // generate county features and projection
-  const projection = d3.geoAlbersUsa().fitSize([width, height], counties);
+  const projection = d3
+    .geoAlbersUsa()
+    .fitSize([width, height], topojson.feature(us, us.objects.counties));
   const path = d3.geoPath(projection);
+
+  // set color scales for availability and usage
+  const color = d3
+    .scaleSequential(d3.extent([0, 1]), d3.interpolateRdPu)
+    .nice();
 
   // append state and county boundary paths to the SVG container
   svg
     .append('g')
     .selectAll('path')
-    .data(counties.features)
-    .join('path')
-    .attr('fill', 'blue')
-    .attr('stroke', 'black')
+    .data(counties)
+    .enter()
+    .append('path')
+    .attr('fill', (county) => color(county.properties.availability))
+    .attr('d', path);
+
+  svg
+    .append('path')
+    .datum(topojson.mesh(us, us.objects.counties), (a, b) => a !== b)
+    .attr('fill', 'none')
+    .attr('stroke', 'white')
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-width', 0.5)
     .attr('d', path);
 }
 
 function init() {
   // load necessary datasets
-  loadData(['usTopo.json', 'broadband.csv', 'stateFips.tsv']).then((result) => {
+  loadData(['usTopo.json', 'broadband.csv']).then((result) => {
     const us = result[0];
     const broadband = result[1];
-    const states = result[2];
+
     // generate cartogram
     let counties = topojson.feature(us, us.objects.counties);
-    counties = fixDiscrepancies({ counties, broadband });
-    console.log(generateCountyData({ counties, broadband, states }));
-    createCartogram(counties);
+    counties = generateCountyData({ counties, broadband });
+    console.log(counties);
+    createCartogram({ us, counties });
   });
 }
 
